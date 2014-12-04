cef5ed2f6766c3acca5bffa1756e0c9a5934328d
Zammstein <sammeyer1994@hotmail.com>
2014-12-04 15:06:24 +0100

    first test commit

---
+++ src/main/java/JenkinsGitTest/JenkinsGitTest/App.java
@@ -0,0 +1,13 @@
+package JenkinsGitTest.JenkinsGitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
