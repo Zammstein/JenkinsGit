9f9e3bd56fc1276a5057707eede1177f004b937d
Zammstein <sammeyer1994@hotmail.com>
2014-12-06 15:03:03 +0100

    added calcutaleSomeThings Class

---
+++ src/main/java/JenkinsGitTest/JenkinsGitTest/CalculateSomeThings.java
@@ -0,0 +1,10 @@
+package JenkinsGitTest.JenkinsGitTest;
+
+public class CalculateSomeThings {
+
+	public int result;
+	
+	public CalculateSomeThings(int numberOne, int numberTwo) {
+		result = numberOne + numberTwo;
+	}
+}
