ffadf9e141f7041dc0d4aff6237ef1d888d07bf8
Zammstein <sammeyer1994@hotmail.com>
2014-12-07 20:34:20 +0100

    commint on 7/12/14

--- src/main/java/JenkinsGitTest/JenkinsGitTest/App.java
+++ src/main/java/JenkinsGitTest/JenkinsGitTest/App.java
@@ -7,7 +7,7 @@ package JenkinsGitTest.JenkinsGitTest;
 public class App {
     public static void main( String[] args ) {
         System.out.println("Hello World!");
-        CalculateSomeThings calc = new CalculateSomeThings(10,22);
+        CalculateSomeThings calc = new CalculateSomeThings(1,22);
         System.out.println(calc.result);
     }
 }
